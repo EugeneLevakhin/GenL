ab7f8f8decac7a27eb95f6ad5392c429b1d61a7f
Eugene Levakhin <eugene.levakhin@gmail.com>
2022-10-20 19:05:46 +0300

    Initial Visual Studio template, .gitignore changed
    Visual Studio template for ASP.NET Core Web App (Model-View-Controller) with configuration: .NET6, configure for HTTPS, Individual Accounts Authentication type,
    .gitignore changed/generated with "dotnet new gitignore" command

---
+++ GenL.Presentation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GenL.Presentation.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
